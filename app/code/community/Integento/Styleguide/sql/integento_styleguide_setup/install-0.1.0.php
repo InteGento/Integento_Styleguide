f6d9eadc7a2e8f242b1c990d6fe48a38b4655c81
Kevin <kevin@rocher-dev.com>
2016-10-17 18:07:38 +0200

    add installer

--- app/code/community/Integento/Styleguide/sql/integento_styleguide_setup/install-0.1.0.php
+++ app/code/community/Integento/Styleguide/sql/integento_styleguide_setup/install-0.1.0.php
@@ -9,19 +9,101 @@
  * @copyright Copyright (c) 2016 Integento (http://github.com/InteGento)
  */
 
+
 try {
 
-    /* @var $conn Varien_Db_Adapter_Interface */
-    /* @var $installer Mage_Core_Model_Resource_Setup */
-    $installer = $this;
-    $installer->startSetup();
+    $_html = <<<EOT
+<reference name="content">
+    <action method="unsetChild"><alias>cms.wrapper</alias></action>
+    <block type="core/template" name="page" as="page" template="integento_styleguide/page.phtml" />
+</reference>
+EOT;
 
+    $cmsPagesToCreateData = array(
+        array(
+            'title' => 'Integento Styleguide',
+            'identifier' => 'styleguide',
+            'layout_update_xml' => $_html
+        )
+    );
 
+    /* Get stores
+    -------------------------- */
 
+    $stores = Mage::app()->getStores();
+    $_stores = array();
+    foreach ($stores as $store) {
+        $_storeId = $store->getId();
+        $_stores[] = array(
+            'id' => $_storeId,
+            'locale' => Mage::getStoreConfig('general/locale/code', $_storeId)
+        );
+    }
 
-    $installer->endSetup();
+    /* CMS ID
+    -------------------------- */
+
+    $cmsDefault = array(
+        'title' => 'Default page',
+        'content_heading' => '',
+        'meta_keywords' => '',
+        'meta_description' => '',
+        'root_template' => 'one_column',
+        'identifier' => 'default-id',
+        'content' => '',
+        'layout_update_xml' => '',
+        'is_active' => true,
+        'stores' => array(Mage_Core_Model_App::ADMIN_STORE_ID),
+        'sort_order' => 0
+    );
+
+    $cmsPages = array();
+    foreach ($cmsPagesToCreateData as $data) {
+        /* Create one page by store */
+        if (isset($data['integento_multistore']) && $data['integento_multistore']) {
+            unset($data['integento_multistore']);
+            foreach ($_stores as $_store) {
+                $data['stores'] = array($_store['id']);
+                $cmsPages[] = $data;
+            }
+        } else {
+            $cmsPages[] = $data;
+        }
+    }
+
+    foreach ($cmsPages as $data) {
+        $cmsPage = Mage::getModel('cms/page');
+        $data = array_merge($cmsDefault, $data);
+
+        if (isset($data['autofill_content']) && $data['autofill_content']) {
+            $data['content_heading'] = $data['title'];
+            $data['content'] = '<p>' . $data['title'] . '</p>';
+        }
+
+        if (isset($data['stores']) && is_array($data['stores']) && count($data['stores']) == 1) {
+            $_storeId = $data['stores'][0];
+            $cmsPage->setStore($_storeId)->load($data['identifier'], 'identifier');
+            $pageId = $cmsPage->checkIdentifier($data['identifier'], $_storeId);
+        } else {
+            $cmsPage->load($data['identifier'], 'identifier');
+            $pageId = $cmsPage->getId();
+        }
+
+        if (!$pageId) {
+            // Create CMS Page if it doesn't exist
+            $cmsPage->addData($data);
+        } else {
+            // Update CMS Page
+            $data['page_id'] = $pageId;
+            $cmsPage->setData($data);
+        }
+
+        $cmsPage->save();
+    }
 
 } catch (Exception $e) {
-    // Silence is golden
-    //throw $e;
+    Mage::logException($e);
+    if (Mage::getIsDeveloperMode()) {
+        Mage::throwException($e->getMessage());
+    }
 }
